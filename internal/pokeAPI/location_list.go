c0c789da25c33451c40241693924acb7602674c1
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-10-27 18:08:43 -0500

    Use the cache when using the pokeAPI to get location data

--- internal/pokeAPI/location_list.go
+++ internal/pokeAPI/location_list.go
@@ -13,24 +13,28 @@ func (c *Client) ListLocations(pageURL string) (RespLocations, error) {
 		url = pageURL
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return RespLocations{}, err
-	}
+	data, ok := c.cache.Get(url)
+	if !ok {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return RespLocations{}, err
+		}
 
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return RespLocations{}, fmt.Errorf("error with get request's response: %w", err)
-	}
-	defer res.Body.Close()
+		res, err := c.httpClient.Do(req)
+		if err != nil {
+			return RespLocations{}, fmt.Errorf("error with get request's response: %w", err)
+		}
+		defer res.Body.Close()
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return RespLocations{}, fmt.Errorf("error reading from response body: %w", err)
+		data, err = io.ReadAll(res.Body)
+		if err != nil {
+			return RespLocations{}, fmt.Errorf("error reading from response body: %w", err)
+		}
+		c.cache.Add(url, data)
 	}
 
 	locResp := RespLocations{}
-	err = json.Unmarshal(data, &locResp)
+	err := json.Unmarshal(data, &locResp)
 	if err != nil {
 		return RespLocations{}, fmt.Errorf("error unmarshalling pokeapi location json: %w", err)
 	}
